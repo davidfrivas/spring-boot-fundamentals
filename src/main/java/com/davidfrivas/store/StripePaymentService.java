1a3f3cb660a22b8cc498ffc100ffe6bf9e386060
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-18 17:47:26 -0400

    Make Stripe the primary payment service

--- src/main/java/com/davidfrivas/store/StripePaymentService.java
+++ src/main/java/com/davidfrivas/store/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.davidfrivas.store;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service("stripe")
+@Primary // Stripe is the primary or default payment service
 public class StripePaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
