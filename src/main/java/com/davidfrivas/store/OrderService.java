9547fd63945a08e3537e04e0de451cdaec0fa477
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-18 14:15:13 -0400

    Add setter method for dependency injection

--- src/main/java/com/davidfrivas/store/OrderService.java
+++ src/main/java/com/davidfrivas/store/OrderService.java
@@ -4,6 +4,7 @@ public class OrderService {
     // Private field to store payment service
     private PaymentService paymentService;
 
+    // Constructor injection
     // Pass our dependency as an argument to this constructor
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
@@ -11,5 +12,10 @@ public class OrderService {
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    // Setter injection: setter method for dependency injection
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
