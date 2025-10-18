ec07569af6c284a8ccd7bbd6e3b6479a19848010
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-18 17:45:29 -0400

    Make PayPal a qualifier for payment service

--- src/main/java/com/davidfrivas/store/OrderService.java
+++ src/main/java/com/davidfrivas/store/OrderService.java
@@ -1,12 +1,17 @@
 package com.davidfrivas.store;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service // Manage objects of type OrderService
 public class OrderService {
     // Private field to store payment service
     private PaymentService paymentService;
 
     // Constructor injection
     // Pass our dependency as an argument to this constructor
-    public OrderService(PaymentService paymentService) {
+    // Use PayPal payment service
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
