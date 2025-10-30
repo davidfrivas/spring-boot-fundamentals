b8a6d01732aef0e0b88d15b71cef4363289e01ab
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-29 22:35:42 -0400

    Re-initialize

--- src/main/java/com/davidfrivas/store/StoreApplication.java
+++ src/main/java/com/davidfrivas/store/StoreApplication.java
@@ -10,12 +10,6 @@ public class StoreApplication {
 	public static void main(String[] args) {
 		// the `run` method returns an object of type ApplicationContext (IoC container that stores objects)
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-		// Get a bean of type UserService managed by Spring
-		var userService = context.getBean(UserService.class);
-		userService.registerUser(new User(1L, "johndoe@gmail.com", "password123", "John Doe"));
-		//userService.registerUser(new User(1L, "johndoe@gmail.com", "password123", "John Doe"));
-
 	}
 
 }
