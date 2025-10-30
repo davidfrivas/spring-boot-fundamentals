a69bf7ee03e9d6d2eb69c065c6c74464ddace320
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-30 00:17:07 -0400

    Change null constraints of columns

---
+++ src/main/resources/db/migration/V6__change_profiles_null_constraints.sql
@@ -0,0 +1,12 @@
+alter table profiles
+    modify bio text not null;
+
+alter table profiles
+    modify phone_number varchar(15) not null;
+
+alter table profiles
+    modify date_of_birth date not null;
+
+alter table profiles
+    modify loyalty_points int unsigned default '0' not null;
+
