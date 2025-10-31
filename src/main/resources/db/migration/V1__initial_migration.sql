db8a430093939eaa496b069a63fb21b8491002c5
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-10-31 02:05:58 -0400

    Reformat SQL keywords to uppercase

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -1,196 +1,197 @@
 -- User table
-create table user
+CREATE TABLE user
 (
     user_id    bigint auto_increment
-        primary key,
-    username   varchar(255)                       not null unique,
-    name       varchar(255)                       not null,
-    email      varchar(255)                       not null unique,
-    password   varchar(255)                       not null,
-    lab_id     bigint                             not null,
-    role       varchar(255)                       not null,
-    created_at datetime default current_timestamp not null,
-    updated_at datetime default current_timestamp on update current_timestamp null
+        PRIMARY KEY,
+    username   varchar(255)                       NOT NULL UNIQUE,
+    name       varchar(255)                       NOT NULL,
+    email      varchar(255)                       NOT NULL UNIQUE,
+    password   varchar(255)                       NOT NULL,
+    lab_id     bigint                             NOT NULL,
+    role       varchar(255)                       NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL
 );
 
 -- Lab table
-create table lab
+CREATE TABLE lab
 (
     lab_id        bigint auto_increment
-        primary key,
-    name          varchar(255)                       not null,
-    contact_email varchar(255)                       not null,
-    institution   varchar(255)                       not null,
-    department    varchar(255)                       not null,
-    address       varchar(255)                       not null,
-    description   text                               not null,
-    created_at    datetime default current_timestamp not null,
-    updated_at    datetime default current_timestamp not null on update current_timestamp
+        PRIMARY KEY,
+    name          varchar(255)                       NOT NULL,
+    contact_email varchar(255)                       NOT NULL,
+    institution   varchar(255)                       NOT NULL,
+    department    varchar(255)                       NOT NULL,
+    address       varchar(255)                       NOT NULL,
+    description   text                               NOT NULL,
+    created_at    datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- Add lab-id FK constraint to User table
-alter table user add constraint fk_user_lab_id
-    foreign key (lab_id) references lab (lab_id)
-        on delete restrict -- Don't delete labs with users
-        on update cascade;
+ALTER TABLE user
+    ADD CONSTRAINT fk_user_lab_id
+        FOREIGN KEY (lab_id) REFERENCES lab (lab_id)
+            ON DELETE RESTRICT -- Don't delete labs with users
+            ON UPDATE CASCADE;
 
 -- Research Protocol table
-create table research_protocol
+CREATE TABLE research_protocol
 (
     protocol_id     bigint auto_increment
-        primary key,
-    protocol_number varchar(255)                            not null unique,
-    title           text                                    not null,
-    description     text                                    not null,
-    lab_id          bigint                                  not null,
-    status          varchar(255)                            not null,
-    approval_date   date                                    null,
-    expiration_date date as (date_add(approval_date, interval 1 year)) stored,
-    created_at      datetime default current_timestamp      not null,
-    updated_at      datetime default current_timestamp      not null on update current_timestamp,
-    constraint fk_protocol_lab_id
-        foreign key (lab_id) references lab (lab_id)
-            on delete restrict  -- Don't allow deleting labs with active protocols
+        PRIMARY KEY,
+    protocol_number varchar(255)                       NOT NULL UNIQUE,
+    title           text                               NOT NULL,
+    description     text                               NOT NULL,
+    lab_id          bigint                             NOT NULL,
+    status          varchar(255)                       NOT NULL,
+    approval_date   date NULL,
+    expiration_date date AS (date_add(approval_date, INTERVAL 1 YEAR)) stored,
+    created_at      datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_protocol_lab_id
+        FOREIGN KEY (lab_id) REFERENCES lab (lab_id)
+            ON DELETE RESTRICT -- Don't allow deleting labs with active protocols
 );
 
 -- Lab-Protocol junction table
-create table protocol_personnel
+CREATE TABLE protocol_personnel
 (
-    protocol_id bigint                             not null,
-    user_id     bigint                             not null,
-    role        varchar(255)                       not null,
-    assigned_at datetime default current_timestamp not null,
-    constraint pk_protocol_personnel
-        primary key (protocol_id, user_id),
-    constraint fk_protocol_personnel_protocol
-        foreign key (protocol_id) references research_protocol (protocol_id)
-            on delete cascade, -- If protocol deleted, remove all personnel assignments
-    constraint fk_protocol_personnel_user
-        foreign key (user_id) references user (user_id)
-            on delete cascade -- If user deleted, remove from all protocols
+    protocol_id bigint                             NOT NULL,
+    user_id     bigint                             NOT NULL,
+    role        varchar(255)                       NOT NULL,
+    assigned_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT pk_protocol_personnel
+        PRIMARY KEY (protocol_id, user_id),
+    CONSTRAINT fk_protocol_personnel_protocol
+        FOREIGN KEY (protocol_id) REFERENCES research_protocol (protocol_id)
+            ON DELETE CASCADE, -- If protocol deleted, remove all personnel assignments
+    CONSTRAINT fk_protocol_personnel_user
+        FOREIGN KEY (user_id) REFERENCES user (user_id)
+            ON DELETE CASCADE  -- If user deleted, remove from all protocols
 );
 
 -- Mouse table
-create table mouse
+CREATE TABLE mouse
 (
     mouse_id      bigint auto_increment
-        primary key,
-    name          varchar(255)                       not null,
-    sex           enum ('M', 'F')         null,
-    genotype      varchar(255)                       not null,
-    strain        varchar(255)                       not null,
-    date_of_birth date                               not null,
-    availability  boolean  default true              not null,
-    notes         text                               null,
-    lab_id        bigint                             not null,
-    protocol_id   bigint                             not null,
-    user_id       bigint                             not null,
-    mother_id     bigint                             null,
-    father_id     bigint                             null,
-    created_at    datetime default current_timestamp not null,
-    updated_at    datetime default current_timestamp not null on update current_timestamp,
-    constraint fk_mouse_father_id
-        foreign key (father_id) references mouse (mouse_id)
-            on delete set null, -- If father is deleted, set to NULL
-    constraint fk_mouse_lab_id
-        foreign key (lab_id) references lab (lab_id)
-            on delete restrict,  -- Don't delete labs with mice
-    constraint fk_mouse_mother_id
-        foreign key (mother_id) references mouse (mouse_id)
-            on delete set null, -- If mother is deleted, set to NULL
-    constraint fk_mouse_protocol_id
-        foreign key (protocol_id) references research_protocol (protocol_id)
-            on delete restrict, -- Don't delete protocols with active mice
-    constraint fk_mouse_user_id
-        foreign key (user_id) references user (user_id)
-            on delete restrict -- Don't delete users who have mice assigned
+        PRIMARY KEY,
+    name          varchar(255)                       NOT NULL,
+    sex           enum ('M', 'F')         NULL,
+    genotype      varchar(255)                       NOT NULL,
+    strain        varchar(255)                       NOT NULL,
+    date_of_birth date                               NOT NULL,
+    availability  boolean  DEFAULT TRUE              NOT NULL,
+    notes         text NULL,
+    lab_id        bigint                             NOT NULL,
+    protocol_id   bigint                             NOT NULL,
+    user_id       bigint                             NOT NULL,
+    mother_id     bigint NULL,
+    father_id     bigint NULL,
+    created_at    datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_mouse_father_id
+        FOREIGN KEY (father_id) REFERENCES mouse (mouse_id)
+            ON DELETE SET NULL, -- If father is deleted, set to NULL
+    CONSTRAINT fk_mouse_lab_id
+        FOREIGN KEY (lab_id) REFERENCES lab (lab_id)
+            ON DELETE RESTRICT, -- Don't delete labs with mice
+    CONSTRAINT fk_mouse_mother_id
+        FOREIGN KEY (mother_id) REFERENCES mouse (mouse_id)
+            ON DELETE SET NULL, -- If mother is deleted, set to NULL
+    CONSTRAINT fk_mouse_protocol_id
+        FOREIGN KEY (protocol_id) REFERENCES research_protocol (protocol_id)
+            ON DELETE RESTRICT, -- Don't delete protocols with active mice
+    CONSTRAINT fk_mouse_user_id
+        FOREIGN KEY (user_id) REFERENCES user (user_id)
+            ON DELETE RESTRICT  -- Don't delete users who have mice assigned
 );
 
 -- Litter junction table
-create table litter
+CREATE TABLE litter
 (
     litter_id     bigint auto_increment
-        primary key,
-    lab_id        bigint not null,
-    mother_id     bigint not null,
-    father_id     bigint not null,
-    date_of_birth date   not null,
-    protocol_id   bigint    not null,
-    notes         text   null,
-    created_at    datetime default current_timestamp not null,
-    updated_at    datetime default current_timestamp on update current_timestamp not null,
-    constraint fk_litter_father_id
-        foreign key (father_id) references mouse (mouse_id)
-            on delete restrict,  -- Don't delete breeding mice
-    constraint fk_litter_lab_id
-        foreign key (lab_id) references lab (lab_id)
-            on delete restrict,
-    constraint fk_litter_mother_id
-        foreign key (mother_id) references mouse (mouse_id)
-            on delete restrict,
-    constraint fk_litter_protocol_id
-        foreign key (protocol_id) references research_protocol (protocol_id)
-            on delete restrict
+        PRIMARY KEY,
+    lab_id        bigint                             NOT NULL,
+    mother_id     bigint                             NOT NULL,
+    father_id     bigint                             NOT NULL,
+    date_of_birth date                               NOT NULL,
+    protocol_id   bigint                             NOT NULL,
+    notes         text NULL,
+    created_at    datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at    datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT fk_litter_father_id
+        FOREIGN KEY (father_id) REFERENCES mouse (mouse_id)
+            ON DELETE RESTRICT, -- Don't delete breeding mice
+    CONSTRAINT fk_litter_lab_id
+        FOREIGN KEY (lab_id) REFERENCES lab (lab_id)
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_litter_mother_id
+        FOREIGN KEY (mother_id) REFERENCES mouse (mouse_id)
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_litter_protocol_id
+        FOREIGN KEY (protocol_id) REFERENCES research_protocol (protocol_id)
+            ON DELETE RESTRICT
 );
 
 -- Log Entry table
-create table log_entry
+CREATE TABLE log_entry
 (
     log_id     bigint auto_increment
-        primary key,
-    user_id    bigint                             null,
-    lab_id     bigint                             not null,
-    content    text                               not null,
-    created_at datetime default current_timestamp not null,
-    constraint fk_log_entry_lab
-        foreign key (lab_id) references lab (lab_id)
-            on delete restrict, -- Restrict to preserve logs
-    constraint fk_log_entry_user
-        foreign key (user_id) references user (user_id)
-            on delete set null -- Preserve logs even if user deleted
+        PRIMARY KEY,
+    user_id    bigint NULL,
+    lab_id     bigint                             NOT NULL,
+    content    text                               NOT NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT fk_log_entry_lab
+        FOREIGN KEY (lab_id) REFERENCES lab (lab_id)
+            ON DELETE RESTRICT, -- Restrict to preserve logs
+    CONSTRAINT fk_log_entry_user
+        FOREIGN KEY (user_id) REFERENCES user (user_id)
+            ON DELETE SET NULL  -- Preserve logs even if user deleted
 );
 
 -- Mouse Log Entry junction table
-create table mouse_log_entry
+CREATE TABLE mouse_log_entry
 (
-    log_id   bigint not null,
-    mouse_id bigint not null,
+    log_id   bigint NOT NULL,
+    mouse_id bigint NOT NULL,
 
-    constraint pk_mouse_log_entry
-        primary key (log_id, mouse_id),  -- Composite PK prevents duplicates
+    CONSTRAINT pk_mouse_log_entry
+        PRIMARY KEY (log_id, mouse_id), -- Composite PK prevents duplicates
 
-    constraint fk_mouse_log_entry_log
-        foreign key (log_id) references log_entry (log_id)
-            on delete cascade ,  -- If log deleted, remove all associations
+    CONSTRAINT fk_mouse_log_entry_log
+        FOREIGN KEY (log_id) REFERENCES log_entry (log_id)
+            ON DELETE CASCADE,          -- If log deleted, remove all associations
 
-    constraint fk_mouse_log_entry_mouse
-        foreign key (mouse_id) references mouse (mouse_id)
-            on delete cascade  -- If mouse deleted, remove from all logs
+    CONSTRAINT fk_mouse_log_entry_mouse
+        FOREIGN KEY (mouse_id) REFERENCES mouse (mouse_id)
+            ON DELETE CASCADE           -- If mouse deleted, remove from all logs
 );
 
 -- Mouse Request table
-create table mouse_request
+CREATE TABLE mouse_request
 (
     request_id   bigint auto_increment
-        primary key,
-    requestor_id bigint                             not null,
-    mouse_id     bigint                             not null,
-    from_lab_id  bigint                             not null,
-    to_lab_id    bigint                             not null,
-    message      text                               not null,
-    status       varchar(255)                       not null,
-    created_at   datetime default current_timestamp not null,
-    updated_at   datetime default current_timestamp not null on update current_timestamp,
-    constraint fk_from_lab_id
-        foreign key (from_lab_id) references lab (lab_id)
-            on delete cascade,
-    constraint fk_mouse_id
-        foreign key (mouse_id) references mouse (mouse_id)
-            on delete cascade,
-    constraint fk_requestor_id
-        foreign key (requestor_id) references user (user_id)
-            on delete cascade,
-    constraint fk_to_lab_id
-        foreign key (to_lab_id) references lab (lab_id)
-            on delete cascade
+        PRIMARY KEY,
+    requestor_id bigint                             NOT NULL,
+    mouse_id     bigint                             NOT NULL,
+    from_lab_id  bigint                             NOT NULL,
+    to_lab_id    bigint                             NOT NULL,
+    message      text                               NOT NULL,
+    status       varchar(255)                       NOT NULL,
+    created_at   datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at   datetime DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_from_lab_id
+        FOREIGN KEY (from_lab_id) REFERENCES lab (lab_id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_mouse_id
+        FOREIGN KEY (mouse_id) REFERENCES mouse (mouse_id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_requestor_id
+        FOREIGN KEY (requestor_id) REFERENCES user (user_id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_to_lab_id
+        FOREIGN KEY (to_lab_id) REFERENCES lab (lab_id)
+            ON DELETE CASCADE
 );
